ab0789bff72e4f39b6323f3093d8c38ac98e2ab1
Emil Roding <emilroding27873@gmail.com>
2026-09-08 10:51:19 +1200

    Finished Priority Queue Assignment

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -3,56 +3,75 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 // TODO Problem 2 - Write and run test cases and fix the code to match requirements.
 
 [TestClass]
+
 public class PriorityQueueTests
 {
-    // Scenario: Create a queue with the following people, turns & priorities: Bob (2, 1), Tim (5, 2), Sue (3, 3) and
-    // run until the queue is empty
-    // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: The Personqueue class was using a List instead of a Queue. 
-    public void TestTakingTurnsQueue_FiniteRepetition()
+    [TestMethod]
+    // Scenario: Create a queue with the following value & priorities: Jimmy (3), Tim (1), Sue (2) Remove the items from the queue and verify that they are returned in order of priority (highest to lowest).
+    // Expected Result: Jimmy, Sue, Tim
+    // Defect(s) Found: Incorrect Loop to find the highest priority item
+    public void TestPriorityQueue()
     {
-        var bob = new PriorityQueue.PriorityItem("Bob", 2, 1);
-        var tim = new PriorityQueue.PriorityItem("Tim", 5, 2);
-        var sue = new PriorityQueue.PriorityItem("Sue", 3, 3);
+        var tim = new PriorityItem("Tim", 1);
+        var sue = new PriorityItem("Sue", 2);
+        var jimmy = new PriorityItem("Jimmy", 3);
 
-        Person[] expectedResult = [bob, tim, sue, bob, tim, sue, tim, sue, tim, tim];
+        PriorityItem[] expectedResult = [jimmy, sue, tim];
 
-        var players = new TakingTurnsQueue();
-        players.AddPerson(bob.Name, bob.Turns);
-        players.AddPerson(tim.Name, tim.Turns);
-        players.AddPerson(sue.Name, sue.Turns);
+        var players = new PriorityQueue();
+        players.Enqueue("Tim", 1);
+        players.Enqueue("Sue", 2);
+        players.Enqueue("Jimmy", 3);
 
-        int i = 0;
-        while (players.Length > 0)
+        for (int i = 0; i < expectedResult.Length; i++)
         {
-            if (i >= expectedResult.Length)
-            {
-                Assert.Fail("Queue should have ran out of items by now.");
-            }
-
-            var person = players.GetNextPerson();
-            Assert.AreEqual(expectedResult[i].Name, person.Name);
-            i++;
+            var actual = players.Dequeue();
+            var expected = expectedResult[i].Value;
+            Assert.AreEqual(expected, actual);
         }
     }
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_1()
+    // Scenario: Create a queue with the following value & priorities: Jimmy (3), Bob (1), Tim (1), Sue (2) Remove the items from the queue and verify that they are returned in order of priority (highest to lowest).  If two items have the same priority then the one that was added first should be returned first.
+    // Expected Result: Jimmy, Sue, Bob, Tim
+    // Defect(s) Found: none
+    public void TestPriorityQueue_MorethanOneItem()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
-    }
+        var bob = new PriorityItem("Bob", 1);
+        var tim = new PriorityItem("Tim", 1);
+        var sue = new PriorityItem("Sue", 2);
+        var jimmy = new PriorityItem("Jimmy", 3);
 
+        PriorityItem[] expectedResult = [jimmy, sue, bob, tim];
+
+        var players = new PriorityQueue();
+        players.Enqueue("Bob", 1);
+        players.Enqueue("Tim", 1);
+        players.Enqueue("Sue", 2);
+        players.Enqueue("Jimmy", 3);
+
+        for (int i = 0; i < expectedResult.Length; i++)
+        {
+            var actual = players.Dequeue();
+            var expected = expectedResult[i].Value;
+            Assert.AreEqual(expected, actual);
+        }
+    }
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
+    // Scenario: Empty queue.  Call Dequeue and verify that an exception is thrown.
+    // Expected Result: InvalidOperationException
     // Defect(s) Found: 
-    public void TestPriorityQueue_2()
+    public void TestPriorityQueue_Empty()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        try
+        {
+            priorityQueue.Dequeue();
+            Assert.Fail("Expected InvalidOperationException was not thrown.");
+        }
+        catch (InvalidOperationException e)
+        {
+           Assert.AreEqual("The queue is empty.", e.Message);
+        }
     }
 
     // Add more test cases as needed below.
