afd464f70e84b097ce6a82be3ca7cf8ad90d32c4
Emil Roding <emilroding27873@gmail.com>
2026-09-07 20:58:03 +1200

    made a start on priorityqueue

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -5,6 +5,36 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 [TestClass]
 public class PriorityQueueTests
 {
+    // Scenario: Create a queue with the following people, turns & priorities: Bob (2, 1), Tim (5, 2), Sue (3, 3) and
+    // run until the queue is empty
+    // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
+    // Defect(s) Found: The Personqueue class was using a List instead of a Queue. 
+    public void TestTakingTurnsQueue_FiniteRepetition()
+    {
+        var bob = new PriorityQueue.PriorityItem("Bob", 2, 1);
+        var tim = new PriorityQueue.PriorityItem("Tim", 5, 2);
+        var sue = new PriorityQueue.PriorityItem("Sue", 3, 3);
+
+        Person[] expectedResult = [bob, tim, sue, bob, tim, sue, tim, sue, tim, tim];
+
+        var players = new TakingTurnsQueue();
+        players.AddPerson(bob.Name, bob.Turns);
+        players.AddPerson(tim.Name, tim.Turns);
+        players.AddPerson(sue.Name, sue.Turns);
+
+        int i = 0;
+        while (players.Length > 0)
+        {
+            if (i >= expectedResult.Length)
+            {
+                Assert.Fail("Queue should have ran out of items by now.");
+            }
+
+            var person = players.GetNextPerson();
+            Assert.AreEqual(expectedResult[i].Name, person.Name);
+            i++;
+        }
+    }
     [TestMethod]
     // Scenario: 
     // Expected Result: 
